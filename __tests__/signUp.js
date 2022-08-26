cda67422b0057ad10d17867548194b5e8eee253a
Renni Nugraheni <renni.nugraheni@bukalapak.com>
2022-08-26 17:54:03 +0700

    add test

--- __tests__/signUp.js
+++ __tests__/signUp.js
@@ -6,7 +6,7 @@ const { matchers } = require('jest-json-schema');
 
 expect.extend(matchers);
 
-describe('test api sign up user', () => {
+describe('positive test api sign up user', () => {
     test('user can successfully sign up using valid data', async () => {
         const response = await ep.epPost(data.dataPost);
         expect(response.status).toEqual(200);
@@ -15,8 +15,13 @@ describe('test api sign up user', () => {
 });
 
 describe('negative test api sign up user', () => {
-    test('user failed sign up using empty password', async () => {
+    test('user failed sign up using empty password and first name', async () => {
         const response = await ep.epPost(data.emptyData);
+        expect(response.status).toEqual(422);
+        expect(response.body).toMatchSnapshot();
+    });
+    test('user failed sign up using duplicate email', async () => {
+        const response = await ep.epPost(data.duplicateEmail);
         expect(response.status).toEqual(422);
         expect(response.body).toMatchSnapshot();
     });
--- data/data.js
+++ data/data.js
@@ -19,6 +19,18 @@ const emptyData = {
         first_name: "",
         last_name: "test",
         password: "",
+        phone_number: "",
+        user_type: "User"
+    }
+};
+
+const duplicateEmail = {
+    user: {
+        currency_id: 2,
+        email: "",
+        first_name: faker.name.firstName(),
+        last_name: "test",
+        password: "testtest",
         phone_number: faker.phone.number('+62-###########'),
         user_type: "User"
     }
@@ -27,4 +39,5 @@ const emptyData = {
 module.exports = {
 dataPost,
 emptyData,
+duplicateEmail,
 };
